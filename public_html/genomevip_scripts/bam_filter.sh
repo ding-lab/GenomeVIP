0cc13f8ca3ae6e864cf014252345ad931f934b4e
Jay Mashl <rmashl@genome.wustl.edu>
2015-12-21 17:12:24 -0600

    keep originals

--- public_html/genomevip_scripts/bam_filter.sh
+++ public_html/genomevip_scripts/bam_filter.sh
@@ -17,7 +17,7 @@ if [ "$MODE" = "somatic" ] ; then
     FAIL_STR1=`awk '{if(NR==2){print $0}}' $BAMLIST `
     grep    '^#' $VARFILE | tee $VARFILE.somfilter_pass > $VARFILE.somfilter_fail
     grep -v '^#' $VARFILE | tee >(grep -v $FAIL_STR1 >> $VARFILE.somfilter_pass) | grep  $FAIL_STR1 >> $VARFILE.somfilter_fail
-    rm -f $VARFILE
+#    rm -f $VARFILE
     
 elif [ "$MODE" = "trio" ] ; then
     FAIL_STR1=`awk '{if(NR==1){print $0}}' $BAMLIST `
@@ -26,7 +26,7 @@ elif [ "$MODE" = "trio" ] ; then
     printf '%s\n'  $FAIL_STR1 $FAIL_STR2 >  $dn.patterns
     grep    '^#' $VARFILE | tee $VARFILE.triofilter_pass > $VARFILE.triofilter_fail
     grep -v '^#' $VARFILE | tee >(grep -v $FAIL_STR1 | grep -v $FAIL_STR2 >> $VARFILE.triofilter_pass) | grep -f $dn.patterns  >> $VARFILE.triofilter_fail
-    rm -f $VARFILE
+#    rm -f $VARFILE
 
 else 
     echo "\nERROR in bam_filter.sh: " $MODE " is not a valid mode."
