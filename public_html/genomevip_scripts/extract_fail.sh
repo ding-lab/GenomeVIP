0aa526e5e2f4480df14f78e8e59dccbebeb5b47e
Jay Mashl <rmashl@genome.wustl.edu>
2016-06-23 14:50:05 -0500

    fix and add i/o enhancement

--- public_html/genomevip_scripts/extract_fail.sh
+++ public_html/genomevip_scripts/extract_fail.sh
@@ -1,14 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env perl 
 # ---------------------
 # @name GenomeVIP utility script to extract failed calls
 # @author R. Jay Mashl <rmashl@genome.wustl.edu>
-# @version
+
+# @version 0.2: change scripting language to reduce i/o
+# @version 0.1: original
 #
 # @syntax extract_fail.sh  orig.vcf  pass.vcf  fail.vcf
 # ---------------------
-export myorig="$1"
-export mypass="$2"
-export myfail="$3"
+# previous version
+#(grep '^#' $mypass  &&  grep -v -f $mypass  $myorig) > $myfail
 
-grep '^#' "$mypass"             > "$myfail"
-comm -2 -3 "$myorig" "$mypass" >> "$myfail"
+use warnings;
+use strict;
+
+my ($myorig, $mypass, $myfail) = @ARGV;
+my %pass;
+
+# read filter lines
+open(IN, "< $mypass");
+  while(<IN>) { $pass{$_}=1; }
+close(IN);
+# process
+open(OUT, "> $myfail");
+  open(IN, "< $myorig");
+    while(<IN>) {
+	if(/^#/ || !exists($pass{$_})){ print OUT $_; }
+    }
+  close(IN);
+close(OUT);
